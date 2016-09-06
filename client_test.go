3aa664f038ff7cfb19f44fcb198db570a1e75a2c
Dan Merritt <djosme@gmail.com>
2016-09-06 23:28:05 +0900

    Add logger tests

--- client_test.go
+++ client_test.go
@@ -1,20 +1,25 @@
 package jwks
 
 import (
+	"bytes"
+	"io"
+	"log"
+	"os"
+	"strings"
 	"testing"
 	"time"
 )
 
-func newConfigSetsDefaults(t *testing.T) {
+func TestNewConfigSetsDefaults(t *testing.T) {
 	config := NewConfig()
-	if config.cacheTimeout != time.Duration(30) ||
+	if config.cacheTimeout != time.Duration(600) ||
 		config.requestTimeout != time.Duration(30) ||
 		config.disableStrictTLS != false {
 		t.Fail()
 	}
 }
 
-func withCacheTimeoutSetsCacheTimeout(t *testing.T) {
+func TestWithCacheTimeoutSetsCacheTimeout(t *testing.T) {
 	config := NewConfig()
 	result := config.WithCacheTimeout(time.Duration(60))
 	if config.cacheTimeout != time.Duration(60) {
@@ -25,7 +30,7 @@ func withCacheTimeoutSetsCacheTimeout(t *testing.T) {
 	}
 }
 
-func withRequestTimeoutSetsRequestTimeout(t *testing.T) {
+func TestWithRequestTimeoutSetsRequestTimeout(t *testing.T) {
 	config := NewConfig()
 	result := config.WithRequestTimeout(time.Duration(42))
 	if config.requestTimeout != time.Duration(42) {
@@ -36,13 +41,60 @@ func withRequestTimeoutSetsRequestTimeout(t *testing.T) {
 	}
 }
 
-func withStrictTLSPolicySetsTLSVerifyPolicy(t *testing.T) {
+func TestWithStrictTLSPolicySetsTLSVerifyPolicy(t *testing.T) {
 	config := NewConfig()
 	result := config.WithStrictTLSPolicy(true)
 	if config.disableStrictTLS != true {
 		t.Fail()
 	}
 	if config != result {
+		t.Fail()
+	}
+}
+
+func TestWithDebugLoggingCustomLogger(t *testing.T) {
+	config := NewConfig()
+	buf := &bytes.Buffer{}
+	logger := log.New(buf, "custom: ", log.LstdFlags)
+	result := config.WithDebugLogging(true, logger)
+
+	if config.enableDebugLogging != true || config.logger != logger || config != result {
+		t.Fail()
+	}
+
+	logger.Println("custom logger")
+	loggedMsg := buf.String()
+	if !strings.HasPrefix(loggedMsg, "custom: ") || !strings.Contains(loggedMsg, "custom logger") {
+		t.Fail()
+	}
+}
+
+func TestWithDebugLoggingStandardLogger(t *testing.T) {
+	oldErr := os.Stderr
+	r, w, _ := os.Pipe()
+	os.Stderr = w
+
+	config := NewConfig()
+	result := config.WithDebugLogging(true, nil)
+
+	if config.enableDebugLogging != true || config != result {
+		t.Fail()
+	}
+
+	config.logger.Println("logged to stderr")
+
+	outC := make(chan string)
+	go func() {
+		var buf bytes.Buffer
+		io.Copy(&buf, r)
+		outC <- buf.String()
+	}()
+
+	w.Close()
+	os.Stderr = oldErr
+	loggedMsg := <-outC
+
+	if !strings.HasPrefix(loggedMsg, "go-jwks: ") || !strings.Contains(loggedMsg, "logged to stderr") {
 		t.Fail()
 	}
 }
